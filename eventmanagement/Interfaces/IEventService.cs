e857eede51910411f45d4086b8218de4cd84a5eb
oendovitsky <ov.endovitsky@yandex.ru>
2026-03-14 15:54:50 +0300

    Реализация EventService.

--- eventmanagement/Interfaces/IEventService.cs
+++ eventmanagement/Interfaces/IEventService.cs
@@ -1,6 +1,13 @@
-﻿namespace eventmanagement.Interfaces
+﻿using eventmanagement.Models;
+
+namespace eventmanagement.Interfaces
 {
     public interface IEventService
     {
+        Event? GetById(Guid id);
+        IEnumerable<Event> GetAll();
+        Guid Add(Event @event);
+        Event Update(Event @event);
+        Guid Delete(Guid id);
     }
 }
--- eventmanagement/Services/EventService.cs
+++ eventmanagement/Services/EventService.cs
@@ -1,8 +1,67 @@
 ﻿using eventmanagement.Interfaces;
+using eventmanagement.Models;
 
 namespace eventmanagement.Services
 {
     public class EventService : IEventService
     {
+        private List<Event> _events =
+        [
+            new Event
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test",
+                Description = "Test",
+                StartAt = DateTime.Now,
+                EndAt = DateTime.Now
+            }
+        ];
+
+        public Guid Add(Event @event)
+        {
+            @event.Id = Guid.NewGuid();
+            _events.Add(@event);
+
+            return @event.Id;
+        }
+
+        public Guid Delete(Guid id)
+        {
+            var @event = _events.FirstOrDefault(x => x.Id == id);
+            if (@event == null)
+            {
+                throw new ArgumentException($"Не найдено событие {id}.");
+            }
+
+            _events.Remove(@event);
+
+            return @event.Id;
+        }
+
+        public IEnumerable<Event> GetAll()
+        {
+            return _events;
+        }
+
+        public Event? GetById(Guid id)
+        {
+            return _events.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Event Update(Event @event)
+        {
+            var currentEvent = _events.FirstOrDefault(x => x.Id == @event.Id);
+            if(currentEvent == null)
+            {
+                throw new ArgumentException($"Не найдено событие {@event.Id}.");
+            }
+
+            currentEvent.Title = @event.Title;
+            currentEvent.Description = @event.Description;
+            currentEvent.StartAt = @event.StartAt;
+            currentEvent.EndAt = @event.EndAt;
+
+            return currentEvent;
+        }
     }
 }
